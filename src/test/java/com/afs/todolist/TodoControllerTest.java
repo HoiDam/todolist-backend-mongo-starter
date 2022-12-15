dd876d625d77223e1e37b27b10e3651abba2328f
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-15 21:16:01 +0800

    test: test delete todo api

--- src/test/java/com/afs/todolist/TodoControllerTest.java
+++ src/test/java/com/afs/todolist/TodoControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,6 +39,17 @@ public class TodoControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].color").value("#ffffff"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].text").value("22"));
 
+    }
+
+    @Test
+    void should_delete_item_by_id_when_perform_item_given_todo_id() throws Exception {
+
+        Todo todo = todoRepository.save(new Todo("Test",false,"#ffffff"));
+
+        //when & then
+        client.perform(MockMvcRequestBuilders.delete("/todos/"+todo.getId()))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+        assertEquals(todoRepository.findAll().size(), 0);
     }
     
 }
